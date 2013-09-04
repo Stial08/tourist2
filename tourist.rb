879861324906694f754f9b31e9a8701c9a96c00a
Stiffen Alejandro Macias Quintero <stiffen.macias@tangosource.com>
2013-09-04 11:31:29 -0500

    require pry and method to ope file

--- tourist.rb
+++ tourist.rb
@@ -1,10 +1,17 @@
-class Tourist
-
+require 'pry'
+class Toznurist
+  def initialize
+  end
 end
 
 class OpenFile
   def open_file
-  file = File.new('/files/sample.input.txt',''r)
+    file = File.new('/files/sample.input.txt',''r)
+    @array = []
+    while line = file.gets
+      @array << file.split(" ") unless line == "\n"
+    end
+  binding.pry
   end
 end
 
@@ -13,3 +20,7 @@ end
 
 class Jenifer < Tourist
 end
+
+##calar 
+  file OpenFile.new
+  file.openfile
